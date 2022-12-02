61d00e39f9fe2b071b7ec671cb308471fbc128ef
liam-oyh <liam.oyh@gmail.com>
2022-12-02 16:34:21 +0000

    test: search output

--- element.sh
+++ element.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 PSQL="psql -X --username=freecodecamp --dbname=periodic_table --tuples-only -c"
 
+MIAN_MENU(){
+  # check if run with arguement
+  if [[ -z $1 ]]
+  then
+    # if no arguement given
+    echo -e "\nPlease provide an element as an argument."
+  # if there is arguement
+  # if the arguement is number
+  else if [[ $1 =~ ^[0-9]+$ ]]
+  then
+    SERCH_RESULT=$($PSQL "SELECT name FROM properties WHERE atomic_number=$1")
+    # if the number matches any atomic_number
+    echo $SERCH_RESULT
+  
+  fi
+}
+
+MAIN_MENU
+
