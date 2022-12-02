2a3b9334da9eee741fe22829e6cc5bc4353040d9
liam-oyh <liam.oyh@gmail.com>
2022-12-02 22:29:40 +0000

    fix: output space issue

--- element.sh
+++ element.sh
@@ -7,12 +7,12 @@ MAIN_MENU(){
   if [[ -z $INPUT ]]
   then
     # if no arguement given
-    echo -e "\nPlease provide an element as an argument."
+    echo Please provide an element as an argument.
   # if there is arguement
   # if the arguement is number
   elif [[ $INPUT =~ ^[0-9]+$ ]]
   then
-    E_NAME=$($PSQL "SELECT name FROM elements WHERE atomic_number=$INPUT")
+    E_NAME=$(echo $($PSQL "SELECT name FROM elements WHERE atomic_number=$INPUT") | sed -r 's/^ *| *$//g')
     # if the number matches any atomic_number
     if [[ -z $E_NAME ]]
     then
@@ -26,12 +26,46 @@ MAIN_MENU(){
       E_TYPE=$(echo $($PSQL "SELECT type FROM properties INNER JOIN types USING(type_id) WHERE atomic_number=$INPUT") | sed -r 's/^ *| *$//g')
 
       # return output
-      echo "The element with atomic_number $INPUT is $E_NAME ($E_SYMBOL). It's a $E_TYPE, with a mass of $E_MASS amu. $E_NAME has a melting point has a melting point of $E_MPOINT celsius and a boiling point of $E_BPOINT celsius."
+      echo "The element with atomic number $INPUT is $E_NAME ($E_SYMBOL). It's a $E_TYPE, with a mass of $E_MASS amu. $E_NAME has a melting point of $E_MPOINT celsius and a boiling point of $E_BPOINT celsius."
     fi
-  elif [[ echo $INPUT | wc -c <= 2 ]]
-  then
-    echo $INPUT
   else
+    # check if the input string is symbol
+    if [[ ${#INPUT} <=2 ]]
+    then
+       E_SYMBOL=$(echo $INPUT | sed -r 's/^ *| *$//g')
+       E_ATOMICNUM=$(echo $($PSQL "SELECT atomic_number FROM elements WHERE symbol='$INPUT'") | sed -r 's/^ *| *$//g')
+       if [[ -z $E_ATOMICNUM ]]
+       then
+         echo I could not find that element in the database.
+       else
+         # get other properties of the element
+         E_NAME=$(echo $($PSQL "SELECT name FROM elements WHERE atomic_number=$E_ATOMICNUM") | sed -r 's/^ *| *$//g')
+         E_MASS=$(echo $($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number=$E_ATOMICNUM") | sed -r 's/^ *| *$//g')
+         E_MPOINT=$(echo $($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number=$E_ATOMICNUM") | sed -r 's/^ *| *$//g')
+         E_BPOINT=$(echo $($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number=$E_ATOMICNUM") | sed -r 's/^ *| *$//g')
+         E_TYPE=$(echo $($PSQL "SELECT type FROM properties INNER JOIN types USING(type_id) WHERE atomic_number=$E_ATOMICNUM") | sed -r 's/^ *| *$//g')
+
+         # return output
+         echo "The element with atomic number $E_ATOMICNUM is $E_NAME ($E_SYMBOL). It's a $E_TYPE, with a mass of $E_MASS amu. $E_NAME has a melting point of $E_MPOINT celsius and a boiling point of $E_BPOINT celsius."
+       fi
+    else
+       E_NAME=$(echo $INPUT | sed -r 's/^ *| *$//g')
+       E_SYMBOL=$(echo $($PSQL "SELECT symbol FROM elements WHERE name='$INPUT'") | sed -r 's/^ *| *$//g')
+       if [[ -z $E_SYMBOL ]]
+       then
+         echo I could not find that element in the database.
+       else
+         # get other properties of the element
+         E_ATOMICNUM=$(echo $($PSQL "SELECT atomic_number FROM elements WHERE name='$INPUT'") | sed -r 's/^ *| *$//g')
+         E_MASS=$(echo $($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number=$E_ATOMICNUM") | sed -r 's/^ *| *$//g')
+         E_MPOINT=$(echo $($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number=$E_ATOMICNUM") | sed -r 's/^ *| *$//g')
+         E_BPOINT=$(echo $($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number=$E_ATOMICNUM") | sed -r 's/^ *| *$//g')
+         E_TYPE=$(echo $($PSQL "SELECT type FROM properties INNER JOIN types USING(type_id) WHERE atomic_number=$E_ATOMICNUM") | sed -r 's/^ *| *$//g')
+
+         # return output
+         echo "The element with atomic number $E_ATOMICNUM is $E_NAME ($E_SYMBOL). It's a $E_TYPE, with a mass of $E_MASS amu. $E_NAME has a melting point of $E_MPOINT celsius and a boiling point of $E_BPOINT celsius."
+        fi
+    fi
     
   fi
 }
